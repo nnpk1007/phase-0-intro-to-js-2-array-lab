617f9fa97e7577be792b73b9e5acfa97fcd200ff
Ngoc Nguyen <nnpk0410@gmail.com>
2023-02-27 19:23:00 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const new_cats = [...cats, name]
+    return new_cats
+}
+
+function prependCat(name) {
+    const new_cats = [name, ...cats];
+    return new_cats;
+}
+
+function removeLastCat() {
+    const new_cats = [...cats];
+    new_cats.pop()
+    return new_cats;
+}
+
+function removeFirstCat() {
+    const new_cats = [...cats];
+    new_cats.shift();
+    return new_cats;
+}
